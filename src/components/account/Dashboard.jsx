b97299d2f3a9c83b1b828a6e771917c683726ff5
Your Name <you@example.com>
2024-03-29 20:39:26 +0000

    updated dashboard

--- src/components/account/Dashboard.jsx
+++ src/components/account/Dashboard.jsx
@@ -10,6 +10,13 @@ import { FaRegTrashAlt } from "react-icons/fa";
 
 const Dashboard = () => {
   const restaurant = useSelector(selectRestaurant);
+
+  let total = 0;
+  restaurant.forEach((restaurant) => {
+    if (restaurant.favourite) {
+      total++;
+    }
+  });
   const dispatch = useDispatch();
   const favourites = useSelector(selectFavourites);
   return (
@@ -26,25 +33,26 @@ const Dashboard = () => {
       <div className="results">
         <div className="favouritesDashboard">
           <h1>Favourites Dashboard</h1>
+          <h2>Total favourited restaurants: {total}</h2>
           {favourites &&
             favourites.map((favourite, index) => {
               return (
                 <div className="container" key={index}>
                   <p>{new Date(favourite.date).toDateString()}</p>
                   <h1>{favourite.name}</h1>
-                  <img src={favourite.image} style={{ width: "35vw" }} />
+                  <span>
+                    <img src={favourite.image} style={{ width: "35vw" }} />{" "}
+                    <button
+                      className="remove"
+                      onClick={() => {
+                        dispatch(setRemove(favourite.id));
+                      }}
+                    >
+                      <FaRegTrashAlt className="trash" />
+                    </button>
+                  </span>
                   <div className="restaurantInfo">
-                    <p>
-                      What did you like? {favourite.review}{" "}
-                      <button
-                        className="remove"
-                        onClick={() => {
-                          dispatch(setRemove(favourite.id));
-                        }}
-                      >
-                        <FaRegTrashAlt className="trash" />
-                      </button>
-                    </p>
+                    <p>What did you like? {favourite.review} </p>
                   </div>
 
                   <div className="line"></div>
